fa87d1906dbe1ebd765c3e5634937a60bda9ff09
jonathanmenezesm <jonathanmenezesm@gmail.com>
2025-11-12 09:35:47 -0300

    desafio71

--- desafio71.py
+++ desafio71.py
@@ -1,1 +1,13 @@
 # Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues. OBS: Considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
+
+print('''-----------------------------------------
+    Seja bem vindo ao Jony's Bank
+-----------------------------------------''')
+
+saque = int(input('Digite o valor que você deseja sacar: '))
+cedulas = [50, 20, 10, 1]
+for valor in cedulas:
+    quantidade = saque // valor
+    saque = saque - (quantidade * valor)
+    if quantidade > 0:
+        print(f'{quantidade} cédula(s) de R${valor}')
