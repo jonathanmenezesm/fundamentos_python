951be94abe2efe841f800343c231198a9c011bc2
jonathanmenezesm <jonathanmenezesm@gmail.com>
2025-11-23 17:28:13 -0300

    Lista: exercicios 78 concluido

--- desafio78.py
+++ desafio78.py
@@ -1,2 +1,10 @@
 # Faça um programa que leia 5 valores numéricos e guarde-os em uma lista.
 # No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.
+
+lista = []
+for n in range (0,5):
+    a = input('Digite um número: ')
+    lista.append(a)
+print(f'A Lista é: {lista} com {len(lista)} elementos')
+print(f'O maior valor é: {max(lista)} na posição {lista.index(max(lista))}')
+print(f'O menor valor é: {min(lista)} na posição {lista.index(min(lista))}')
