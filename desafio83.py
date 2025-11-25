5f78e751ae3fe1be0fd293cd3515dafe6e96d0f9
jonathanmenezesm <jonathanmenezesm@gmail.com>
2025-11-25 10:45:40 -0300

    Lista: exercicios 83 concluido

--- desafio83.py
+++ desafio83.py
@@ -1,1 +1,7 @@
 # 💡 Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
+
+expressao = input('Digite uma expressão matemática com parentesis ex.: (a+b) x 2: ')
+if '(' and ')' in expressao:
+    print('Esta expressão é valida!')
+else:
+    print('Expressão invalida!')
