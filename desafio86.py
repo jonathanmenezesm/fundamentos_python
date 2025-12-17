d6596372d93ac618f0504015c41abc9ae5b17661
jonathanmenezesm <jonathanmenezesm@gmail.com>
2025-12-17 10:08:53 -0300

    Lista: desafio 86 finalizado

--- desafio86.py
+++ desafio86.py
@@ -14,3 +14,18 @@
 # # [ 1 ][ 2 ][ 3 ]
 # # [ 4 ][ 5 ][ 6 ]
 # # [ 7 ][ 8 ][ 9 ]
+
+# Criação da matriz 3x3 inicializada com zeros
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+# Preenchendo a matriz com valores lidos pelo teclado
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz[l][c] = int(input(f"Digite um valor para [{l}, {c}]: "))
+
+# Exibindo a matriz formatada
+print("\nMatriz:")
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f"[ {matriz[l][c]:^5} ]", end="")
+    print()  # quebra de linha ao final de cada linha da matriz
