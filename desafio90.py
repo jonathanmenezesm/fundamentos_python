7a803f1e5e080f1a2f3e80ee930e0e8322071bfb
jonathanmenezesm <jonathanmenezesm@gmail.com>
2025-12-22 11:49:34 -0300

    Dicionario: desafio 90 concluido

--- desafio90.py
+++ desafio90.py
@@ -1,1 +1,14 @@
 #Faça um programa que leia nome e media de um aluno, guardando também a situação em um dicionário. No final mostre o conteúdo da estrutura na tela.
+
+# 1 - iniciando dicionario
+aluno = dict()
+# 2 - lendo os dados
+nome = str(input('Qual o seu nome: '))
+media = float(input('Qual a sua média: '))
+
+# 3 adicionando no dicionario
+aluno['nome'] = nome
+aluno['media'] = media
+
+# 4 exibindo dict
+print(aluno)
