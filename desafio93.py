353d4f340182724c4d5cc51ca936c3539a6bfcd7
jonathanmenezesm <jonathanmenezesm@gmail.com>
2025-12-23 10:26:40 -0300

    Dicionario: desafio 93 concluido

--- desafio93.py
+++ desafio93.py
@@ -2,3 +2,12 @@
 # O programa vai ler o nome do jogador e quantas partidas ele jogou.
 # Depois vai ler a quantidade de gols feitos em cada partida.
 # No final, tudo isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.
+
+resumo = dict()
+nome = input('Qual o nome do jogador: ')
+n_partidas = int(input('Quantas partidas o jogou: '))
+gols_partidas = int(input('Quantos gols nas partidas: '))
+resumo['nome'] = nome
+resumo['n_partidas'] = n_partidas
+resumo['gols_partidas'] = gols_partidas
+print(resumo)
