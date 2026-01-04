53bfa17756a62cb23e89212500188a72b1877740
jonathanmenezesm <jonathanmenezesm@gmail.com>
2026-01-04 00:12:56 -0300

    Função: desafio 103 concluido

--- desafio103.py
+++ desafio103.py
@@ -1,2 +1,9 @@
 # Faça um programa que tenha uma função chamada ficha(), que receba dois parâmetros opcionais: o nome de um jogador e quantos gols ele marcou.
 # O programa deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado não tenha sido informado corretamente.
+
+def ficha(nome='<desconhecido>', gols=0):
+    print(f'O jogador {nome} fez {gols} gols nesta temporada.')
+
+ficha()
+ficha("Jony")
+ficha("Jony", 3)
