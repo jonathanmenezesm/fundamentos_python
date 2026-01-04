26fdd4c850cdd1a7d1285dbe60cd64f7419a9d05
jonathanmenezesm <jonathanmenezesm@gmail.com>
2026-01-04 01:41:50 -0300

    Função: desafio 105 refatorado

--- desafio105.py
+++ desafio105.py
@@ -6,52 +6,28 @@
 # - A situação (opcional)
 # Adicione também as docstrings da função.
 
-def notas(*n, situacao=False):
+def notas(*args, situacao=False):
     """
-    Função notas()
-    Recebe várias notas de alunos e retorna um dicionário com informações:
-    - Quantidade de notas
-    - Maior nota
-    - Menor nota
-    - Média da turma
-    - Situação (opcional, baseada na média: >=7 Aprovado, <7 Reprovado)
-
-    Retorna:
-    dict: dicionário com os dados calculados
+    Função notas(*args, situacao=False)
+    Recebe várias notas e retorna um dicionário com:
+    - total de notas
+    - maior nota
+    - menor nota
+    - média da turma
+    - situação (opcional, se situacao=True)
     """
 
-    dados_lista = []
-    dados_dicionario = {}
-    quantidade = int(input('Quantos notas você quer adicionar: '))
-    contador = 0
+    n = dict()
+    n['total'] = len(args)
+    n['maior'] = max(args)
+    n['menor'] = min(args)
+    n['media'] = sum(args) / len(args)
+    if situacao:
+        if n['media'] >= 7:
+            n['status'] = 'APROVADO'
+        else:
+            n['status'] = 'REPROVADO'
+    return n
 
-    while contador < quantidade:
-        nota = float(input('Informe a nota do aluno: '))
-        dados_lista.append(nota)
-        contador += 1
-
-    #definindo tamanha, maior e menor nota
-    numero_notas = len(dados_lista)
-    maior_nota = max(dados_lista)
-    menor_nota = min(dados_lista)
-
-    #adicionando ao dicionario
-    dados_dicionario['numero_notas'] = numero_notas
-    dados_dicionario['maior_nota'] = maior_nota
-    dados_dicionario['menor_nota'] = menor_nota
-
-    resultado = sum(dados_lista) / len(dados_lista)
-    dados_dicionario['media'] = resultado
-
-    #definindo status
-    if dados_dicionario['media'] >= 7:
-        dados_dicionario['situacao'] = 'Aprovado'
-    else:
-        dados_dicionario['situacao'] = 'Reprovado'
-
-    return dados_dicionario
-
-print(notas())
-
-
-
+resumo = notas(10, 9, 9, 0, situacao=True)
+print(resumo)
