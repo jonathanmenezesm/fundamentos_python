9e1792b6c8d9dc0fba45e065d2da2d18aafe87b8
jonathanmenezesm <jonathanmenezesm@gmail.com>
2026-01-04 23:28:11 -0300

    Função: desafio 106 concluido

--- desafio106.py
+++ desafio106.py
@@ -1,3 +1,29 @@
 # Faça um mini-sistema que utilize o Interactive Help do Python. O usuário vai digitar o comando e o manual vai aparecer. Quando o usuário digitar a palavra 'FIM', o programa se encerrará.
 #
 # OBS: use cores.
+{
+
+}
+
+from time import sleep
+
+def escreva(texto):
+    tamanho = len(texto) + 4
+    print("~" * tamanho)
+    print(f"  {texto}")
+    print("~" * tamanho)
+
+def pyhelp(comando):
+    help(comando)
+
+def ajudinha():
+    while True:
+        escreva("Sistema de ajuda PyHELP")
+        comando = input("Função ou Biblioteca > ")
+        if comando.upper() == "FIM":
+            break
+        else:
+            sleep(1)
+            pyhelp(comando)
+
+ajudinha()
